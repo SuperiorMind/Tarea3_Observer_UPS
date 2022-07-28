f878ff4273ab5203ce6e4e66189a6100104016bc
Jimmy <jredrovan5@gmail.com>
2022-07-28 07:11:27 -0500

    Adding observer for currency and a locale system

--- src/oscarblancarte/ipd/observer/ObserverMain.java
+++ src/oscarblancarte/ipd/observer/ObserverMain.java
@@ -2,47 +2,54 @@ package oscarblancarte.ipd.observer;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import java.util.Observable;
 import oscarblancarte.ipd.observer.impl.ConfigurationManager;
+import oscarblancarte.ipd.observer.impl.observers.CurrencyObserver;
 import oscarblancarte.ipd.observer.impl.observers.DateFormatObserver;
 import oscarblancarte.ipd.observer.impl.observers.MoneyFormatObserver;
 
 /**
  * @author Oscar Javier Blancarte Iturralde
- * @see http://www.oscarblancarteblog.com
+ *
  */
 public class ObserverMain {
 
     public static void main(String[] args) {
         ConfigurationManager conf = ConfigurationManager.getInstance();
-        
+
         //Se establecen los valores por default.
         conf.setDefaultDateFormat(new SimpleDateFormat("yyyy/MM/dd"));
         conf.setMoneyFormat(new DecimalFormat("##.00"));
         System.out.println("Established configuration");
-        
+
         //Se dan de alta lo observers
         DateFormatObserver dateFormatObserver = new DateFormatObserver();
         MoneyFormatObserver moneyFormatObserver = new MoneyFormatObserver();
+        CurrencyObserver currencyObserver = new CurrencyObserver();
         conf.addObserver(dateFormatObserver);
         conf.addObserver(moneyFormatObserver);
-        System.out.println("");
+        conf.addObserver(currencyObserver);
+
         
-        //Se cambia la fonfiguratión
+        //Se cambia la configuration
         conf.setDefaultDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
         conf.setMoneyFormat(new DecimalFormat("###,#00.00"));
-        System.out.println("");
+        conf.setSymbolCurrencyFormat(new DecimalFormatSymbols(Locale.US));
         
         //Se realiza otro cambio en la configuración.
         conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy/dd"));
         conf.setMoneyFormat(new DecimalFormat("###,#00"));
-        
+        conf.setSymbolCurrencyFormat(new DecimalFormatSymbols(Locale.FRANCE));
+
         conf.removeObserver(dateFormatObserver);
         conf.removeObserver(moneyFormatObserver);
-        System.out.println("");
+        conf.removeObserver(currencyObserver);
         
         //Se realiza otro cambio en la configuración.
         conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy"));
         conf.setMoneyFormat(new DecimalFormat("###,##0.00"));
+        conf.setSymbolCurrencyFormat(new DecimalFormatSymbols(Locale.JAPAN));
     }
 }
